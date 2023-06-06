e784d5b10c473c09659f66cbbc80a9acae736fd7
joymajumder26 <joymajumder34@gmail.com>
2023-06-06 14:08:00 +0600

    [Done] Created successfully

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application,Request,Response } from "express";
+import cors from 'cors';
+const app:Application = express()
+const port = 3000
+
+//parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//testing
+app.get('/', (req:Request, res:Response) => {
+  res.send('Working successfully')
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database is connected successfully`);
+  } catch (err) {
+    console.log(`Failed to connect database`, err);
+  }
+  app.listen(config.port, () => {
+    console.log(`application  listening on port ${config.port}`);
+  });
+}
+main();
